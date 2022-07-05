6b35cd28703adc432d2e6947d659b1c033b73268
natakers <n.kersnovskaya@gmail.com>
2022-07-05 12:23:16 +0300

    fix: add smoth accordion animation, fix style of code

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,10 @@ console.log(butons);
 
 butons.forEach(item => {
   item.addEventListener('click', () => {
-    item.classList.toggle('activ');
-    const p = item.nextSibling
+    item.classList.toggle('step__icon_activ');
+    const parent = item.parentNode;
+    const parentNext = (parent as HTMLElement).nextElementSibling;
+    const p = (parentNext as HTMLElement).querySelector('.step-container_hide');
+    (parentNext as HTMLElement).classList.toggle('step-container_hide_activ');
   })
 })
-function activ() {
-  
-    
-};
